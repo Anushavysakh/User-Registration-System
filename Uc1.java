91ec9511494745629ac17863a71233f4adfac3a1
Anusha <anushavysakh232@gmail.com>
2022-08-02 22:51:00 +0530

    Uc2 committed

--- Uc1.java
+++ Uc1.java
@@ -3,23 +3,42 @@ package com.day19assignment.bridgelabz;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Uc1 {
-	static String name;
-	Scanner sc=new  Scanner(System.in);
+public class Uc2 {
 
-	public void valideFirstName() {
+	static String firstName,lastName;
+	public void validFirstName() {
 		String nameExp= "^[A-Z]{1}+[a-z]{2,}";
-		System.out.println("Enter first name : ");
-		 name= sc.next(); 
-		if(Pattern.matches(nameExp, name)) {
-	            System.out.println("First Name you Entered is Valid");
-		 }else {
-	            System.out.println("First Name you entered is Invalid");
-		 		}
-	}	 
+		boolean b3 = Pattern.matches(nameExp,firstName);  
+		if(b3!=true) {
+			System.out.println("Invalid Firstname");
+		} else {
+            System.out.println("First Name you entered is Valid");
+
+		}
+	}
+	public void validLastName() {
+		String nameExp= "^[A-Z]{1}+[a-z]{2,}";
+		boolean b3 = Pattern.matches(nameExp,lastName);  
+		if(b3!=true) {
+			System.out.println("Invalid Lastname");
+		}else {
+            System.out.println("Last Name you entered is Valid");
+
+		}
+	}
+	
 	public static void main(String args[]) {
-		Uc1 obj =new Uc1();
-		System.out.println("Welcome to User Registration System");
-		obj.valideFirstName();
+		Uc2 obj =new Uc2();
+		Scanner sc=new  Scanner(System.in);
+		
+		System.out.println("Enter first name : ");
+		firstName = sc.next();
+		obj.validFirstName();
+		System.out.println("Enter last name : ");
+		lastName= sc.next();
+		obj.validLastName();
+		
 	}
 }
+
+
