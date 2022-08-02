d883f3feeabd8361a3612110d4c743c5a57ca88f
Anusha <anushavysakh232@gmail.com>
2022-08-02 22:53:01 +0530

    Uc4 committed

--- Uc1.java
+++ Uc1.java
@@ -3,12 +3,28 @@ package com.day19assignment.bridgelabz;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Uc3 {
+public class Uc4 {
 
-	static String firstName, lastName, email;
+	static String number, firstName, lastName, email, password;
+	Scanner sc = new Scanner(System.in);
 
+	void validateNumber() {
+		String numberExp = "^(\\+91[\\-\\s]?)?[0]?(91)?[789]\\d{9}$";
+		System.out.println("Enter Phone number:");
+		number = sc.next();
+		boolean b3 = Pattern.matches(numberExp, number);
+		if (b3 != true) {
+			System.out.println("The Phone Number you Entered is Invalid");
+		}else {
+            System.out.println("Phone number you entered is Valid");
+
+		}
+	}
+	
 	void validateFirstName() {
 		String nameExp = "^[A-Z]{1}+[a-z]{2,}";
+		System.out.println("\nEnter first name : ");
+		firstName = sc.next();
 		boolean b3 = Pattern.matches(nameExp, firstName);
 		if (b3 != true) {
 			System.out.println("Invalid Firstname");
@@ -20,6 +36,8 @@ public class Uc3 {
 
 	void validateLastName() {
 		String nameExp = "^[A-Z]{1}+[a-z]{2,}";
+		System.out.println("Enter last name : ");
+		lastName = sc.next();
 		boolean b3 = Pattern.matches(nameExp, lastName);
 		if (b3 != true) {
 			System.out.println("Invalid Lastname");
@@ -30,32 +48,31 @@ public class Uc3 {
 	}
 
 	void validateEmail() {
-		String emailExp = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
-		boolean b3 = Pattern.matches(emailExp, email);
-		if (b3 != true) {
-			System.out.println("Invalid Mail Id");
-		}else {
-            System.out.println("Last Name you entered is Valid");
-
-		}
-	}
-
-
-	public static void main(String args[]) {
-		Uc3 obj = new Uc3();
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter first name : ");
-		firstName = sc.next();
-		obj.validateFirstName();
 		
-		System.out.println("Enter last name : ");
-		lastName = sc.next();
+			String emailExp = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
+			System.out.println("Enter Email Id : ");
+			email = sc.next();
+			boolean b3 = Pattern.matches(emailExp, email);
+			if (b3 != true) {
+				System.out.println("Invalid Mail Id");
+			}else {
+	            System.out.println("Email you entered is Valid");
+
+			}
+	}
+		
+	public static void main(String args[]) {
+		Uc4 obj = new Uc4();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to User Registration System");
+		
+		obj.validateFirstName();
+			
 		obj.validateLastName();
 
-		System.out.println("Enter Email Id : ");
-		email = sc.next();
 		obj.validateEmail();
+				
+		obj.validateNumber();
 	}
-
 }
